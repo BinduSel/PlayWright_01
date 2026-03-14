a173c47c986c61bc486a91b8bf75e5335d73770e
BinduSel <shivabindu09@gmail.com>
2026-03-14 05:43:34 +0530

    pdp changes

--- tests/iPhonebuyflow.test.js
+++ tests/iPhonebuyflow.test.js
@@ -3,6 +3,9 @@ import {test} from '@playwright/test'
 import { productsPage } from '../pages/products.page'
 import { iPhoneFLPPage } from '../pages/iPhoneFLP.page'
 
+import data from '../testdata/data.json'
+import { pdpPage } from '../pages/pdp.page';
+
 //
 // Refine your label or accessible name to ensure it matches only one element.
 // Use getByRole() for better targeting based on the element’s role and name.
@@ -12,12 +15,14 @@ import { iPhoneFLPPage } from '../pages/iPhoneFLP.page'
 let page;
 let products;
 let flp;
+let pdp;
 
 test.beforeEach(async ({browser})=>{
 
 page = await browser.newPage()
-products = new productsPage(page);
-flp = new iPhoneFLPPage(page);
+products = new productsPage(page, data.iPhoneName);
+flp = new iPhoneFLPPage(page, data.iPhoneName);
+pdp = new pdpPage(page, data.iPhoneColor);
 
 await products.launchUrl();
 
@@ -29,6 +34,6 @@ test('verify iphone flow', async()=>{
 
     await products.iPhoneModel();
     await flp.goToiPhonePDP();
-    
+    await pdp.selectConfigs();
 
 })
